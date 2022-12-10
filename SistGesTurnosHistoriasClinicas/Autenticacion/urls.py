8be4f6061ed3e60ad1ac995db46aeedc89439702
facutoledo <facufole@gmail.com>
2022-12-10 18:24:09 -0300

    20221210 Estilo de páginas con bootstrap4

--- SistGesTurnosHistoriasClinicas/Autenticacion/urls.py
+++ SistGesTurnosHistoriasClinicas/Autenticacion/urls.py
@@ -5,34 +5,31 @@ from .forms import *
 from .views import *
 
 urlpatterns = [
-    path('ingreso/', views.LoginView.as_view(template_name='ingreso.html', success_url=reverse_lazy('bienvenida')),
+     path('ingreso/', views.LoginView.as_view(template_name='ingreso.html', success_url=reverse_lazy('bienvenida')),
          name='ingreso'),
-    path('salir/', views.LogoutView.as_view(template_name='salir.html'),
+     path('salir/', views.LogoutView.as_view(template_name='salir.html'),
          name='salir'),
-
-    path('contrasena_cambio/hecho/', views.PasswordChangeDoneView.as_view(template_name='contrasena_cambio_hecho.html'),
-         name='contrasena_cambio_hecho'),
-    path('contrasena_cambio/', views.PasswordChangeView.as_view(template_name='contrasena_cambio_form.html',
-                                                                success_url=reverse_lazy('contrasena_cambio_hecho')),
+     path('registro_personal/', RegistroPersonalViews.as_view(template_name="registro_empleados.html"),
+         name='registro_personal'),
+   
+     #Reestablecer la contraseña del usuario logueado
+     path('contrasena_cambio/', views.PasswordChangeView.as_view(template_name='contrasena_cambio_form.html',
+                                                                success_url=reverse_lazy('activar_cambio_contraseña_completado')),
          name='contrasena_cambio'),
 
-    #las siguientes 4 direcciones son para reestablecer la contraseña del usuario vía correo electrónico
-    path('activar_cambio_contraseña/', 
+     #Reestablecer la contraseña del usuario vía correo electrónico
+     path('activar_cambio_contraseña/', 
           #views.PasswordResetView
           ActivarUsuarioCambioContraseña.as_view(template_name="activar_cambio_contraseña.html", success_url=reverse_lazy('activar_cambio_contraseña_hecho')),
           name='activar_cambio_contraseña'),
-    path('activar_cambio_contraseña/hecho/', 
+     path('activar_cambio_contraseña/hecho/', 
           views.PasswordResetDoneView.as_view(template_name="activar_cambio_contraseña_hecho.html"),
           name='activar_cambio_contraseña_hecho'),
-    path('activar_cambio_contraseña/<uidb64>/<token>/', 
+     path('activar_cambio_contraseña/<uidb64>/<token>/', 
           views.PasswordResetConfirmView.as_view(template_name="activar_cambio_contraseña_confirmacion.html", 
                form_class=EstablecerContraseñaForm, success_url=reverse_lazy('activar_cambio_contraseña_completado')),
           name='activar_cambio_contraseña_confirmacion'),
-    path('activar_cambio_contraseña/completado/', 
+     path('activar_cambio_contraseña/completado/', 
           views.PasswordResetCompleteView.as_view(template_name="activar_cambio_contraseña_completado.html"),
           name='activar_cambio_contraseña_completado'),
-
-
-    path('registro_personal/', RegistroPersonalViews.as_view(template_name="registro_empleados.html"),
-         name='registro_personal'),
 ]
